ea05b1ade61bb3a55b41721705762bb03c1f9eb9
Vincent <vincent.wong@hotmail.co.nz>
2018-03-29 01:53:04 +1300

    Added Main and Settings Page for testing. Testing Reset Preferences in settings as well as formatting.

---
+++ HelloWorld/HelloWorld/HelloWorld/Settings.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Settings : ContentPage
+	{
+
+        public Boolean NightModeButton;
+
+        public Settings()
+        {
+            InitializeComponent();
+            NightModeButton = this.FindByName<SwitchCell>("NightMode").IsEnabled;
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            if (NightModeButton == true)
+            {
+                this.FindByName<SwitchCell>("NightMode").On = false;
+            }
+        }
+    }
+}
